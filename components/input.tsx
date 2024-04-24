59afe160569d037873295216928be7e8d0eb8251
muriloxz77 <muriloaugusto9374@gmail.com>
2024-04-23 21:38:27 -0300

    criação do cabeçalho

--- components/input.tsx
+++ components/input.tsx
@@ -1,39 +1,47 @@
 import React from 'react';
-import { TextInput, Text, View, StyleSheet } from 'react-native';
+import { TextInput, Text, View, StyleSheet, TextInputProps, useColorScheme } from 'react-native';
+import  useTheme from "../temas/Temas";
 
-const InputField = ({ label, value, setValue, placeholder }) => {
-  return (
-    <View>
-      <Text style={styles.text}>{label}</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setValue}
-        value={value}
-        placeholder={placeholder}
-      />
-    </View>
-  );
-};
+interface InputFieldProps extends TextInputProps{
+  label: string;
+}
 
-const styles = StyleSheet.create({
+
+export default function InputField({ label, placeholder, secureTextEntry }: InputFieldProps)  {
+  const [hover, sethover] = React.useState('#f5f5f5');
+  const cores = useTheme();
+  const styles = StyleSheet.create({
   input: {
     height: 40,
     width: '95%',
     margin: 12,
     padding: 10,
-    borderWidth: 0,
-    borderColor: 'black',
-    backgroundColor: '#f5f5f5',
+    borderWidth: 1,
+    borderColor: hover,
+    backgroundColor: cores.bgPrimaryVariant,
     borderRadius: 10,
-
   },
   text: {
     fontSize: 14,
-    color: 'black',
+    color: cores.textColor,
+    
     marginTop: 10,
     marginLeft: 25,
   },
-});
+  });
+  return (
+    <View>
+      <Text style={styles.text}>{label}</Text>
+        <TextInput
+          style={styles.input}
+          placeholder={placeholder}
+          secureTextEntry={secureTextEntry}
+          placeholderTextColor={cores.textColorVariant}
+          onFocus={() => sethover("#f39200")}
+          onBlur={() => sethover("#f5f5f5")}
+        />
+    </View>
+  );
+};
 
-export default InputField;
 
